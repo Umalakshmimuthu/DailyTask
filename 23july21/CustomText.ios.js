c27bbeb418455390f9fd48e2c8959022b5a97b2c
Umalakshmimuthu <75730228+Umalakshmimuthu@users.noreply.github.com>
2021-07-24 04:25:31 -0700

    Delete 23july21 directory

--- 23july21/CustomText.ios.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-import Styles from "./Styles";
-
-const CustomText = (props) => {
-  return <Styles />;
-};
-
-export default CustomText;
